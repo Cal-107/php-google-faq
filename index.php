528b4781f5fc005b70e54dadd505194353ff8678
“Cal-107” <kaly___7@hotmail.it>
2021-12-20 15:13:57 +0100

    add exercise

--- index.php
+++ index.php
@@ -31,5 +31,56 @@ $faqs = [
         'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
         'answer' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ],
-]
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <header>
+        <section class="sec-1 d-flex">
+            <div class="logo-box">
+                <img src="./img/Google_2015_logo.svg" alt="Google Logo">
+            </div>
+            <span class="privacy-terms">Privacy & termini</span>
+        </section>
+        <section class="list">
+            <ul class="d-flex">
+                <li>
+                    Introduzione
+                </li>
+                <li>
+                    Norme sulla privacy
+                </li>
+                <li>
+                    Termini di servizio
+                </li>
+                <li>
+                    Tecnologie
+                </li>
+                <li>
+                    Domande frequenti
+                </li>
+            </ul>
+        </section>
+    </header>
+
+    <main>
+        <section class="container">
+            <?php
+            foreach ($faqs as $faq) {
+                ?>
+                <h1> <?php echo $faq['question']; ?> </h1>
+                <p> <?php echo $faq['answer']; ?> </p>
+            <?php } ?>
+        </section>
+    </main>
+</body>
+</html>
